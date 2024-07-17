33fb9f69c9c91b3b9b8ff1da1226794076f80e7f
Arvin Buid <arvinbuid01@gmail.com>
2024-07-17 19:40:20 +0800

    Installed prettier using npm to automatically format PHP files when it is saved and added a prettierrc config file to customized autoformat.
    Refactor the homepage codes into partials that is named head, navbar, showcase-search, top-banner, bottom-banner and footer.php. Created a loadView and loadPartial helpers to make the imports look cleaner.

--- helpers.php
+++ helpers.php
@@ -2,12 +2,48 @@
 
 /**
  * Get the base path
- * 
+ *
  * @param string $path
  * @return string
  */
 
- function basePath($path = '')
- {
-    return __DIR__ . '/' . $path;
- }
+function basePath($path = '')
+{
+  return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view
+ *
+ * @param string $view
+ * @return void
+ */
+
+function loadView($view)
+{
+  $viewPath = basePath("views/{$view}.view.php");
+
+  if (file_exists($viewPath)) {
+    require $viewPath;
+  } else {
+    echo "File {$view} does not exist.";
+  }
+}
+
+/**
+ * Load a partial
+ *
+ * @param string $partial
+ * @return void
+ */
+
+function loadPartial($partial)
+{
+  $loadPath = basePath("views/partials/{$partial}.php");
+
+  if (file_exists($loadPath)) {
+    require $loadPath;
+  } else {
+    echo "File {$partial} does not exist.";
+  }
+}
