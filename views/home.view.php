11cfe3c2de9f1dbe41b41c02d7a6dd1f748114c3
Arvin Buid <arvinbuid01@gmail.com>
2024-07-19 12:29:57 +0800

    Deleted the hardcoded job listings inside listings/index view and replace it with dynamic job listings that are coming from the database just like in the home view.

--- views/home.view.php
+++ views/home.view.php
@@ -7,7 +7,7 @@
 <section>
   <div class="container mx-auto p-4 mt-4">
     <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
 
       <?php foreach ($listings as $listing): ?>
         <!-- Job Listings -->
@@ -40,11 +40,12 @@
          </div>
        </div>
       <?php endforeach; ?>
-    </div>
-   <a href="/listings" class="block text-xl text-center">
-     <i class="fa fa-arrow-alt-circle-right"></i>
-     Show All Jobs
-   </a>
+     </div>
+    <a href="/listings" class="block text-xl text-center">
+      <i class="fa fa-arrow-alt-circle-right"></i>
+      Show All Jobs
+    </a>
+  </div>
 </section>
 
 
