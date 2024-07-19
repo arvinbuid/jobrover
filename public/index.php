eba27f3ddc549200c4bcbf1eff22eb49b60a9a62
Arvin Buid <arvinbuid01@gmail.com>
2024-07-19 19:24:19 +0800

    Removed the custom autoloader inside public/index.php and installed composer file to replace it with psr-4 autoloader.
    Refactored the Database and Router file to use namespace so that the home, index, show controller and public/index.php file can use it.

--- public/index.php
+++ public/index.php
@@ -1,13 +1,16 @@
 <?php
 require '../helpers.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+use Framework\Router;
 
 // Custom autoloader
-spl_autoload_register(function ($class) {
-  $path = basePath('Framework/' . $class . '.php');
-  if (file_exists($path)) {
-    require $path;
-  }
-});
+// spl_autoload_register(function ($class) {
+//   $path = basePath('Framework/' . $class . '.php');
+//   if (file_exists($path)) {
+//     require $path;
+//   }
+// });
 
 // Instantiate the router
 $router = new Router();
