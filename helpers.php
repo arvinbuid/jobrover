4784b9accc244bd8ac68334bbaf77beb93a82b44
Arvin Buid <arvinbuid01@gmail.com>
2024-07-20 16:48:41 +0800

    Implement a post request for the listings inside routes.php. Added action property inside the form in create.view.php file. Added a store method inside the ListingController class and created a sanitize helper function inside the helpers.php file.

--- helpers.php
+++ helpers.php
@@ -88,3 +88,15 @@ function formatSalary($salary)
 {
   return '₱' . number_format(floatval($salary));
 }
+
+/**
+ * Sanitize Data
+ *
+ * @param string $dirty
+ * @return string
+ */
+
+function sanitize($dirty)
+{
+  return filter_var($dirty, FILTER_SANITIZE_SPECIAL_CHARS);
+}
