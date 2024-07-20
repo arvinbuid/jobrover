cee8304c538e4efc9e03c56841fdae1ed918f164
Arvin Buid <arvinbuid01@gmail.com>
2024-07-21 01:45:51 +0800

    Created an update() method inside the ListingController Class to add update functionality on the website.
    Created a hidden input inside the edit view to make a PUT request. Added success Flash message inside the listings/show.view.php.

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -216,4 +216,101 @@ class ListingController
       'listing' => $listing,
     ]);
   }
+
+  /**
+   * Update a listing
+   *
+   * @param array $params
+   * @return void
+   */
+
+  public function update($params)
+  {
+    $id = $params['id'];
+
+    $params = [
+      'id' => $id,
+    ];
+
+    $listing = $this->db
+      ->query('SELECT * FROM listings WHERE id = :id', $params)
+      ->fetch();
+
+    if (!$listing) {
+      ErrorController::notFound('Listing not found.');
+      return;
+    }
+
+    $allowedFields = [
+      'title',
+      'description',
+      'salary',
+      'tags',
+      'company',
+      'address',
+      'city',
+      'state',
+      'phone',
+      'email',
+      'requirements',
+      'benefits',
+    ];
+
+    $updateValues = [];
+
+    $updateValues = array_intersect_key($_POST, array_flip($allowedFields));
+
+    $updateValues = array_map('sanitize', $updateValues);
+
+    $requiredFields = [
+      'title',
+      'description',
+      'salary',
+      'email',
+      'city',
+      'state',
+    ];
+
+    $errors = [];
+
+    foreach ($requiredFields as $field) {
+      if (
+        empty($updateValues[$field]) ||
+        !Validation::string($updateValues[$field])
+      ) {
+        $errors[$field] = ucfirst($field) . ' is required.';
+      }
+    }
+
+    if (!empty($errors)) {
+      loadView('listings/edit', [
+        'errors' => $errors,
+        'listing' => $listing,
+      ]);
+      exit();
+    } else {
+      // Submit & Update the database
+
+      $updateFields = [];
+
+      foreach (array_keys($updateValues) as $field) {
+        $updateFields[] = "{$field} = :{$field}";
+      }
+
+      $updateFields = implode(', ', $updateFields);
+
+      $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+
+      // Assign id to $updateQuery to replace the placeholder
+      $updateValues['id'] = $id;
+
+      $this->db->query($updateQuery, $updateValues);
+
+      // Flash message
+      $_SESSION['success_message'] = 'Listing updated successfully.';
+
+      // redirect
+      redirect('/listings/' . $id);
+    }
+  }
 }
--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -6,16 +6,18 @@
 <section class="flex justify-center items-center mt-20">
   <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
     <h2 class="text-4xl text-center font-bold mb-4">Edit Job Listing</h2>
-    <form method="POST" action="/listings">
-      <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">Job Info</h2>
+    <form method="POST" action="/listings/<?= $listing->id ?>">
 
+      <!-- Hidden Input-->
+      <input type="hidden" name="_method" value="PUT">
+      <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">Job Info</h2>
       <?php if (isset($errors)): ?>
         <?php foreach ($errors as $error): ?>
-          <div class="bg-red-100 p-2 my-4"><?= $error ?></div>
+          <div class="bg-red-100 p-3 my-4"><?= $error ?></div>
         <?php endforeach; ?>
       <?php endif; ?>
       
-      <div class="mb-4">
+      <div class="mb-4">  
         <input
           type="text"
           name="title"
