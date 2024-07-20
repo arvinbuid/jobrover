99f46e260351f25cb614cf7d0d969b78f8904f3e
Arvin Buid <arvinbuid01@gmail.com>
2024-07-20 22:53:53 +0800

    Created session_start() inside public/index.php file. Created a $_SESSION Superglobal inside destroy() method of ListingController class that contain success_message key.
    Created a flash-message partial to handle success and error flash message. Loaded the flash-message partial inside listings/index.view.php to display a message when a listing is deleted.

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -183,6 +183,9 @@ class ListingController
     // make DELETE command if $listing exists
     $this->db->query('DELETE FROM listings WHERE id = :id', $params);
 
+    // Set flash message
+    $_SESSION['success_message'] = 'Listing deleted successfully.';
+
     redirect('/listings');
   }
 }
