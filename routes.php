cc0d5c516ffa44dd45da95724308ca6e074c57d1
Arvin Buid <arvinbuid01@gmail.com>
2024-07-21 10:56:58 +0800

    Created a user login and register routes inside routes.php file and created a UserController Class file.
    Changed the login and register link in the navbar into /auth routes. Created a login and register views.

--- routes.php
+++ routes.php
@@ -1,13 +1,17 @@
 <?php
 
+// listings GET
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->get('/listings/{id}', 'ListingController@show');
 
+// listings POST, PUT, DELETE
 $router->post('/listings', 'ListingController@store');
-
 $router->put('/listings/{id}', 'ListingController@update');
-
 $router->delete('/listings/{id}', 'ListingController@destroy');
+
+// user Login & Register
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
