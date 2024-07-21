a9598bafa2b3acfcd8ea74775c1fbd5411283fb3
Arvin Buid <arvinbuid01@gmail.com>
2024-07-21 21:50:18 +0800

    Minor spacing modifications inside navbar logout.

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -9,11 +9,11 @@ use Framework\Session; ?>
     <nav class="space-x-4">
       <!-- Check if have user session -->
       <?php if (Session::has('user')): ?>
-      <div class="flex justify-between items-center gap-4">
+      <div class="flex justify-between items-center gap-4 ">
          <div class="text-blue-500">
            Welcome <?php echo Session::get('user')['name']; ?>
          </div>
-         <form method="POST" action="/auth/logout">
+         <form method="POST" action="/auth/logout" class="flex gap-4">
            <button type="submit" class="text-white inline hover:underline">Logout</button>
            <a href="/listings/create"
            class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
