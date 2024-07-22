45d26d02b8e398fcbd8b118902a58e41a849751e
Arvin Buid <arvinbuid01@gmail.com>
2024-07-22 16:20:04 +0800

    Added mobile responsiveness styles inside the navbar and bottom banner.

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -3,20 +3,20 @@ use Framework\Session; ?>
 
 <header class="bg-blue-900 text-white p-4">
   <div class="container mx-auto flex justify-between items-center">
-    <h1 class="text-3xl font-semibold">
+    <h1 class="text-3xl font-semibold left-text">
       <a href="/">Jobrover</a>
     </h1>
     <nav class="space-x-4">
       <!-- Check if have user session -->
       <?php if (Session::has('user')): ?>
       <div class="flex justify-between items-center gap-4 ">
-         <div class="text-blue-500">
+         <div class="text-blue-500 text-base links">
            Welcome <?php echo Session::get('user')['name']; ?>
          </div>
-         <form method="POST" action="/auth/logout" class="flex gap-4">
+         <form method="POST" action="/auth/logout" class="flex gap-4 links">
            <button type="submit" class="text-white inline hover:underline">Logout</button>
            <a href="/listings/create"
-           class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
+           class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300 "
            >
             <i class="fa fa-edit"></i> Post a Job
            </a>
